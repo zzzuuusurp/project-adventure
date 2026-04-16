b181ded86259e267067a7a4dca212dd1391ecb37
zzzuuusurp <zg5e@student.cart.org>
2026-04-16 10:37:51 -0700

    kys (keep yourCode safe)

---
+++ pathchoices.js
@@ -0,0 +1,7 @@
+let log = [];
+let allPathlist = [];
+let allPaths = document.getElementsByClassName('menuC');
+for (const document of allPaths) {
+    console.log(document);
+    allPathlist.push(document);
+}
--- stats.js
+++ stats.js
@@ -2,4 +2,6 @@ let maxhp = 100;
 let hp = 100;
 let maxE = 80;
 let E = 80;
+let atk = 1;
+let def = 5;
 const inv = [];
