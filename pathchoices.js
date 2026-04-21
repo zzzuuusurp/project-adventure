6fea60938ba66f80a24471ea7a1fd9b6661f3e0c
zzzuuusurp <zg5e@student.cart.org>
2026-04-21 09:44:34 -0700

    javascript
    REEE LESASSD ME

--- pathchoices.js
+++ pathchoices.js
@@ -1,7 +1,36 @@
 let log = [];
-let allPathlist = [];
 let allPaths = document.getElementsByClassName('menuC');
-for (const document of allPaths) {
-    console.log(document);
-    allPathlist.push(document);
-}
+let allChoices = document.getElementsByClassName('path');
+
+
+function movePaths(choice) {
+    const currentPathId = document.getElementsByClassName("active")[0].id;
+    const currentPath = document.getElementById(currentPathId);
+    currentPath.classList.remove('active');
+    console.log(currentPath);
+    //saving the text and images for the log
+    const newLog = document.createElement('section');
+    const pee = currentPath.querySelectorAll('p') || false;
+    const imgee = currentPath.querySelectorAll('img') || false;
+    if (pee != false) {
+        for (p in pee) {
+            newLog.appendChild(p);
+        };
+    };
+    if (imgee != false) {
+        for (img in imgee) {
+            newLog.appendChild(imgee);
+        };
+    };
+    log.push(newLog);
+    console.log(log);
+    const path = choice.className;
+    console.log(path);
+    const newPath = document.getElementById(path);
+    newPath.classList.add("active");
+};
+
+for (let index = 0; index < allChoices.length; index++) {
+    const choice = allChoices[index];
+    choice.addEventListener('click', movePaths(choice));
+};
