c9e4d9b58feeb4b7dbe08fc623456cb8dc772548
zzzuuusurp <zg5e@student.cart.org>
2026-04-22 10:35:43 -0700

    CHOOSING WORKS
    HURRAH. HURRAY. I CAN SLACK OFF AT MY TRACK MEET WITH A CLEAR MIND

--- pathchoices.js
+++ pathchoices.js
@@ -1,21 +1,38 @@
 let log = [];
 let allPaths = document.getElementsByClassName('menuC');
 let allChoices = document.getElementsByClassName('path');
+let textBox = document.getElementById('currentSection');
+let hiddenText = document.getElementById('ancientNews')
 //button structure: button.path.OTHERCLASSNAME
-
+let currentPath = 'start';
 console.log(allChoices);
 console.log(allPaths)
 
 function start() {
     if (!document.getElementsByClassName('active')[0]) {
-        document.getElementById('enterName').classList.add('active');
-        document.getElementById('enterName').classList.remove('menuC');
+        nameEntry = document.getElementById('enterName');
+        nameEntry.classList.add('active');
+        nameEntry.classList.remove('menuC');
+        textBox.appendChild(nameEntry);
     }
     
 }
 
-function choiceMade(C) {
-
+function choiceMade(c) {
+    console.log(c);
+    oldNews = document.getElementsByClassName('active')[0];
+    console.log(oldNews);
+    oldNews.classList.add('menuC');
+    oldNews.classList.remove('active');
+    hiddenText.appendChild(oldNews);
+    pathClass = `${c.classList[1]}`;
+    currentPath = pathClass;
+    console.log(pathClass);
+    newPath = document.getElementById(pathClass);
+    console.log(newPath);
+    newPath.classList.add('active');
+    newPath.classList.remove('menuC');
+    textBox.appendChild(newPath);
 }
 
 start();
