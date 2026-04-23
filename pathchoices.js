20c8f40c9a66884d6cf75139a161d09d4ee7712a
zzzuuusurp <zg5e@student.cart.org>
2026-04-23 10:36:54 -0700

    work done
    did work, will do more work.

--- pathchoices.js
+++ pathchoices.js
@@ -1,12 +1,62 @@
+//BORING dom stuff
 let log = [];
-let allPaths = document.getElementsByClassName('menuC');
-let allChoices = document.getElementsByClassName('path');
-let textBox = document.getElementById('currentSection');
-let hiddenText = document.getElementById('ancientNews')
+const allPaths = document.getElementsByClassName('menuC');
+const allChoices = document.getElementsByClassName('path');
+const textBox = document.getElementById('currentSection');
+const hiddenText = document.getElementById('ancientNews');
+const healthBar = document.getElementsByClassName('healthBar');
+const energyBar = document.getElementsByClassName('energyBar');
+const healthCount = document.getElementsByClassName('health');
+const energyCount = document.getElementsByClassName('energy');
+const talonCount = document.getElementsByClassName('talons');
+const everySingleDamnButton = document.querySelectorAll('button');
+const enterName = document.getElementsByClassName('path start')[0];
+const atkBtn = document.getElementsByClassName('attack')[0];
+const defBtn = document.getElementsByClassName('defend')[0];
+const healBtn = document.getElementsByClassName('heal')[0];
 //button structure: button.path.OTHERCLASSNAME
+
+//EPIC AND AWESOME stats
+let maxhp = 100;
+let hp = 100;
+let maxE = 80;
+let E = 80;
+let atk = 1;
+let def = 5;
+let talons = 0;
+let defending = false;
+const inv = [];
+let name = "Guy";
+let elic = "pizza";
+
+//list of STATS for COOL AND INTIMIDATING ENEMIES
+const currentEnemy = {
+
+}
+const encounterList = [
+    {
+        name: 'Robber',
+        image: '#',
+        alt: 'An enemy robber!',
+        maxHp:
+    }
+]
+
+
+function updateBars(HP, H, EP, E, T) {
+    healthCount.innerHTML = `${H} / ${HP}`;
+    energyCount.innerHTML = `${E} / ${EP}`;
+    healthBar.max = HP;
+    healthBar.value = H;
+    energyBar.max = EP;
+    energyBar.value = E;
+    talons.innerHTML = `${T}`;
+}
+
+
 let currentPath = 'start';
 console.log(allChoices);
-console.log(allPaths)
+console.log(allPaths);
 
 function start() {
     if (!document.getElementsByClassName('active')[0]) {
@@ -16,6 +66,12 @@ function start() {
         textBox.appendChild(nameEntry);
     }
     
+}
+
+function savename() {
+    box = document.getElementById('nameEnterer').value || 'Guy';
+    console.log(box);
+    name = `${box}`;
 }
 
 function choiceMade(c) {
@@ -35,15 +91,45 @@ function choiceMade(c) {
     textBox.appendChild(newPath);
 }
 
+function attack(atkStat, defStat, enemyDef, Hp, oppHp, defendingStatus, status, name, luckStat, enemyLuck) {
+    if (!defendingStatus) {
+        const multiplier = Math.round((Math.random() * luckStat) + 1);
+        const damage = (atkStat * multiplier) - enemyDef;
+        if (damage < 0) {damage=0} //falback if defense is higher than attack
+        const remaining = oppHp - damage;
+        status.innerHTML = `${name} Attacks! ${damage} damage dealt!`
+        return remaining;
+    }
+    else if (defendingStatus) {
+        const multiplier = Math.round(Math.random() * enemyLuck + 1);
+        const damage = (enemyDef * multiplier) - defStat;
+        if (damage < 0) {damage=0};
+        const remaining = Hp - damage;
+        status.innerHTML = `${name} is countered! ${damage} damage reflected!`
+        return remaining;
+    }
+}
+
+function defend(defStatus, status, name) {
+    defStatus = !defStatus //sets from true to false and vice versa
+    status.innerHTML = `${name} is on guard.`
+}
+
+function heal(energy, status, Hp) {
+
+}
+
+
+//do work later
+
 start();
-
-
-
+// event listeners
 for (let index = 0; index < allChoices.length; index++) {
     const choice = allChoices[index];
     choice.addEventListener('click', function () {
         choiceMade(choice);
     });
 };
-
-
+enterName.addEventListener('click', function () {
+    savename();
+})
