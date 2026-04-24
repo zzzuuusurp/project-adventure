2fa356ede4a05078ff64ba294b2232aff0d84b9b
zzzuuusurp <zg5e@student.cart.org>
2026-04-24 09:32:44 -0700

    screw tius

--- textStuff.js
+++ textStuff.js
@@ -1,9 +1,10 @@
 story =
 [
     {
-        id: enterName,
+        id: 'enterName',
         text: 'textHere',
         image: 'N/A',
         choice: ['option1', 'option2', 'option3'],
-    }
-]
+        choiceId: ['id1', 'id2', 'id3']
+    },
+];
