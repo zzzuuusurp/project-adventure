dc4617e948cd4deeb30171eb15160adeed248638
zzzuuusurp <zg5e@student.cart.org>
2026-04-27 10:40:10 -0700

    stuff
    work done

--- textStuff.js
+++ textStuff.js
@@ -12,19 +12,19 @@ let talons = 0;
 let healCount = 2;
 
 //functions
-
 function transition(t) {
-    const branch = 'story.' + t;
+    const branch = story[`${t}`];
     console.log(branch);
+    console.log(branch.text);
     textBox.innerHTML = '';
     if (branch.text) {
-        let p = document.createAttribute('p');
+        let p = document.createElement('p');
         p.innerHTML = `${branch.text}`;
         textBox.appendChild(p);
         console.log(p);
     };
     if (branch.img) {
-        let img = document.createAttribute('img');
+        let img = document.createElement('img');
         img.src = `url(${branch.img})`;
         if (branch.alt) {
             img.alt = `${branch.alt}`
@@ -33,13 +33,15 @@ function transition(t) {
         console.log(p);
     }
     if (branch.choice) {
-        let btnArr = document.createAttribute('div');
+        let btnArr = document.createElement('div');
         btnArr.classList.add('choices')
         for (let index = 0; index < branch.choice.length; index++) {
             const btnText = branch.choice[index];
             const path = branch.choiceId[index];
             const button = document.createAttribute('button');
-            button.addEventListener('click', transition(path));
+            button.addEventListener('click', function() {
+                transition(t);
+            });
             button.innerHTML =  btnText;
             console.log(btnArr);
         }
@@ -49,6 +51,7 @@ function transition(t) {
 
 submitName.addEventListener('click', function() {
     name = nameEntry.value || 'Guy';
+    console.log(name)
     transition('start')
 })
 
@@ -57,7 +60,6 @@ submitName.addEventListener('click', function() {
 const story = {
     start: {
          text: 'You are a peasant, walking around the town square. You live a relatively normal life, with your main focus mainly getting by. However, little do you know, one misstep is about to change that. In the middle of your daily walk to work, you come across a vase. Tired of your boring life, you decide to kick it down. The vase shatters with a loud CLASH, breaking it into an uncountable amount of clay pieces A very furious man sees what you\'ve done and comes to you, shouting. He reveals himself to be Bill Gatesford III, the richest man in the world. He angrily informs you that the vase you had "kicked down like a peasant" was worth one million Talons. The very next day, you are confronted by some shady men. They are the debt collectors, and tell you that you have one week before you have to pay the full million you owe.',
-         image: 'N/A',
          choice: ['Negotiate with the collectors', 'Accept your fate and head outside to make some money', 'Slay the collectors with your dimension sword'],
          choiceId: ['hard', 'oldEncounter', 'dimensionSwordEnd']
      },
